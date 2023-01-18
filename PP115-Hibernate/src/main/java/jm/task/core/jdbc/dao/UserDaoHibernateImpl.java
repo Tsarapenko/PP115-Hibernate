e1cefb6bc39deb283f25d7bb5a290fc398df6df7
Tsarapenko <120114047+Tsarapenko@users.noreply.github.com>
2023-01-18 12:01:00 +0300

    First commit

---
+++ PP115-Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,110 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import jm.task.core.jdbc.util.Util;
+import java.util.List;
+
+public class UserDaoHibernateImpl implements UserDao {
+    public UserDaoHibernateImpl() {
+
+    }
+
+
+    @Override
+    public void createUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(" CREATE TABLE IF NOT EXISTS users " +
+                    "(`id` BIGINT NOT NULL AUTO_INCREMENT," +
+                    "`name` VARCHAR(45) NOT NULL," +
+                    "`lastname` VARCHAR(45) NOT NULL," +
+                    "`age` TINYINT NOT NULL," +
+                    "PRIMARY KEY (`id`))").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Проблема с созданием таблицы Hibernate " + e.getStackTrace());
+        }
+    }
+
+
+    @Override
+    public void dropUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Проблема с удалением таблицы Hibernate " + e.getStackTrace());
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try {
+            Session session = Util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+
+            System.out.println("User с именем - " + name + " успешно добавлен в базу данных Hibernate");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Проблема с coхранением user Hibernate " + e.getStackTrace());
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            User user = session.get(User.class, id);
+            transaction = session.beginTransaction();
+      //       session.delete(user);
+            session.createQuery("delete User where id = :id").setParameter("id", id).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Проблема с удалением по id Hibernate " + e.getStackTrace());
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List users;
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            users = session.createQuery("FROM User").list();
+        }
+        return users;
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Проблема с очисткой таблицы" + e.getStackTrace());
+        }
+    }
+}
